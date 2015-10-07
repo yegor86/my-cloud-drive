ccd89842c3a8a89c86df72e6c5c1cbc6da7c5801
starnakin <starnakin@provectus-it.com>
2015-10-08 00:40:12 +0300

    remove api layer

--- src/main/java/org/odesamama/mcd/controllers/UserController.java
+++ src/main/java/org/odesamama/mcd/controllers/UserController.java
@@ -3,22 +3,26 @@ package org.odesamama.mcd.controllers;
 import org.odesamama.mcd.domain.File;
 import org.odesamama.mcd.domain.User;
 import org.odesamama.mcd.repositories.UserRepository;
-import org.odesamama.mcd.restapi.UserResource;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@RequestMapping("/users")
 @RestController
-public class UserController implements UserResource{
+public class UserController{
 	
 	@Resource
 	private UserRepository userRepository;
-	
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public Iterable<User> getUserList() {
           return userRepository.findAll();
     }
 
+    @RequestMapping(value = "/files/{id}", method = RequestMethod.GET)
     public Iterable<File> getUserFileList(@PathVariable("id") Long id) {
         return userRepository.findOne(id).getFileList();
     }
