2929704f285495ef6abb1f41c3cdc32b0f1a96e1
starnakin <starnakin@provectus-it.com>
2016-01-18 22:45:04 +0200

    changes

--- Vagrant-setup/bootstrap_travis.sh
+++ Vagrant-setup/bootstrap_travis.sh
@@ -49,52 +49,52 @@ then
   exit
 fi
 
-PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
-if [ ! -f "$PG_REPO_APT_SOURCE" ]
-then
-  # Add PG apt repo:
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > "$PG_REPO_APT_SOURCE"
-
-  # Add PGDG repo key:
-  wget --quiet -O - https://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
-fi
-
-# Update package list and upgrade all packages
-apt-get update
-apt-get -y upgrade
-
-apt-get -y install "postgresql-$PG_VERSION" "postgresql-contrib-$PG_VERSION"
-
-PG_CONF="/etc/postgresql/$PG_VERSION/main/postgresql.conf"
-PG_HBA="/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
-PG_DIR="/var/lib/postgresql/$PG_VERSION/main"
-
-# Edit postgresql.conf to change listen address to '*':
-sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
-
-# Append to pg_hba.conf to add password auth:
-echo "host    all             all             all                     md5" >> "$PG_HBA"
-
-# Explicitly set default client_encoding
-echo "client_encoding = utf8" >> "$PG_CONF"
-
-# Restart so that all new config is loaded:
-service postgresql restart
-
-cat << EOF | su - postgres -c psql
--- Create the database user:
-CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
-
--- Create the database:
-CREATE DATABASE $APP_DB_NAME WITH OWNER=$APP_DB_USER
-                                  LC_COLLATE='en_US.utf8'
-                                  LC_CTYPE='en_US.utf8'
-                                  ENCODING='UTF8'
-                                  TEMPLATE=template0;
-EOF
-
-# Tag the provision time:
-date > "$PROVISIONED_ON"
+#PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
+#if [ ! -f "$PG_REPO_APT_SOURCE" ]
+#then
+#  # Add PG apt repo:
+#  echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > "$PG_REPO_APT_SOURCE"
+#
+#  # Add PGDG repo key:
+#  wget --quiet -O - https://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
+#fi
+#
+## Update package list and upgrade all packages
+#apt-get update
+#apt-get -y upgrade
+#
+#apt-get -y install "postgresql-$PG_VERSION" "postgresql-contrib-$PG_VERSION"
+#
+#PG_CONF="/etc/postgresql/$PG_VERSION/main/postgresql.conf"
+#PG_HBA="/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
+#PG_DIR="/var/lib/postgresql/$PG_VERSION/main"
+#
+## Edit postgresql.conf to change listen address to '*':
+#sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+#
+## Append to pg_hba.conf to add password auth:
+#echo "host    all             all             all                     md5" >> "$PG_HBA"
+#
+## Explicitly set default client_encoding
+#echo "client_encoding = utf8" >> "$PG_CONF"
+#
+## Restart so that all new config is loaded:
+#service postgresql restart
+#
+#cat << EOF | su - postgres -c psql
+#-- Create the database user:
+#CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
+#
+#-- Create the database:
+#CREATE DATABASE $APP_DB_NAME WITH OWNER=$APP_DB_USER
+#                                  LC_COLLATE='en_US.utf8'
+#                                  LC_CTYPE='en_US.utf8'
+#                                  ENCODING='UTF8'
+#                                  TEMPLATE=template0;
+#EOF
+#
+## Tag the provision time:
+#date > "$PROVISIONED_ON"
 
 echo "Successfully created PostgreSQL dev virtual machine."
 echo ""
