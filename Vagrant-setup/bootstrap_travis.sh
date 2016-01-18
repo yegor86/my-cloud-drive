5a9e6484080f3364142475ca0e2bbfe93b4ff91c
starnakin <starnakin@provectus-it.com>
2016-01-18 23:26:26 +0200

    changes

--- Vagrant-setup/bootstrap_travis.sh
+++ Vagrant-setup/bootstrap_travis.sh
@@ -49,15 +49,15 @@ then
   exit
 fi
 
-PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
-if [ ! -f "$PG_REPO_APT_SOURCE" ]
-then
-  # Add PG apt repo:
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > "$PG_REPO_APT_SOURCE"
-
-  # Add PGDG repo key:
-  wget --quiet -O - https://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
-fi
+#PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
+#if [ ! -f "$PG_REPO_APT_SOURCE" ]
+#then
+#  # Add PG apt repo:
+#  echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > "$PG_REPO_APT_SOURCE"
+#
+#  # Add PGDG repo key:
+#  wget --quiet -O - https://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
+#fi
 
 ## Update package list and upgrade all packages
 apt-get update
