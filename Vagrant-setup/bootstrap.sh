b92293b41f6b138e750eda4e56d1b2a36e3ac64f
starnakin <starnakin@provectus-it.com>
2016-01-18 22:25:29 +0200

    changes

--- Vagrant-setup/bootstrap.sh
+++ Vagrant-setup/bootstrap.sh
@@ -53,7 +53,7 @@ PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
 if [ ! -f "$PG_REPO_APT_SOURCE" ]
 then
   # Add PG apt repo:
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > "$PG_REPO_APT_SOURCE"
+  sudo sh -c  "echo 'deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main' > $PG_REPO_APT_SOURCE"
 
   # Add PGDG repo key:
   wget --quiet -O - https://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
