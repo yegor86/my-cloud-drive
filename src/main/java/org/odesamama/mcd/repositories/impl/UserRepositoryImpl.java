6541e15efe508d4706369659c22783614e9cfd1c
starnakin <starnakin@provectus-it.com>
2016-03-17 16:31:14 +0200

    changed User cunstructor to UserBuilder

--- src/main/java/org/odesamama/mcd/repositories/impl/UserRepositoryImpl.java
+++ src/main/java/org/odesamama/mcd/repositories/impl/UserRepositoryImpl.java
@@ -1,12 +1,13 @@
 package org.odesamama.mcd.repositories.impl;
 
-import java.util.List;
-
 import org.odesamama.mcd.domain.User;
+import org.odesamama.mcd.domain.UserBuilder;
 import org.odesamama.mcd.repositories.CustomUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by starnakin on 27.09.2015.
@@ -21,13 +22,13 @@ public class UserRepositoryImpl implements CustomUserRepository {
     public User findByEmail(String email) {
         List<User> userList = jdbcTemplate.query("select * from public.users where user_email = ?",
                 new Object[] { email }, (rs, rowNum) -> {
-                    User user = new User();
-                    user.setUserId(rs.getLong("user_id"));
-                    user.setUserEmail(email);
-                    user.setUserName(rs.getString("user_name"));
-                    user.setLastName(rs.getString("last_name"));
+                    UserBuilder builder = new UserBuilder();
+                    builder.userId(rs.getLong("user_id"))
+                            .userEmail(email)
+                            .userName(rs.getString("user_name"))
+                            .lastName(rs.getString("last_name"));
 
-                    return user;
+                    return builder.build();
                 });
 
         return !userList.isEmpty() ? userList.get(0) : null;
